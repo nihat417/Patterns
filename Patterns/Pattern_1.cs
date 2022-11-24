cf7f4543b3af1ec61d16fdde8e7c5f99c9be333a
Nihat Akremi <nihatakremi@gmail.com>
2022-11-25 00:07:24 +0300

    Добавьте файлы проекта.

---
+++ Patterns/Pattern_1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns;
+
+interface IChair
+{
+    public void haslegs();
+    public void sitOn();
+}
+
+class VictorianChair: IChair
+{
+    public void haslegs() => Console.WriteLine("victorian chair has legs");
+    public void sitOn() => Console.WriteLine("victorian chair sitOn");
+}
+
+class ModernChair: IChair
+{
+    public void haslegs() => Console.WriteLine("Moddernchair leghs");
+    public void sitOn() => Console.WriteLine("Modern sitOn");
+}
+
+
+
+///
+public interface ICoffeeTable
+{
+    void isRound();
+    void placeOn();
+}
+
+public class VictorianCoffeeTable : ICoffeeTable
+{
+    public void isRound() => Console.WriteLine("Is Round");
+    public void placeOn() => Console.WriteLine($"Placed on victorian cofeetable");
+}
+
+public class ModernCoffeeTable : ICoffeeTable
+{
+    public void isRound() => Console.WriteLine("Is Round");
+    public void placeOn() => Console.WriteLine($"Placed on victorian cofeetable");
+}
+
+
+///
+
+public interface ISofa
+{
+    void haslegs();
+    void sitOn();
+}
+
+class VictorianSofa : ISofa
+{
+    public void haslegs() => Console.WriteLine("sofa has legs");
+    public void sitOn() => Console.WriteLine("sofa sitOn");
+}
+
+class Modernsofa : IChair
+{
+    public void haslegs() => Console.WriteLine("Moddern sofa leghs");
+    public void sitOn() => Console.WriteLine("Modern sofa sitOn");
+}
+
+interface FurnitureFactory
+{
+    public IChair createChair();
+    public  ISofa createSofa();
+    public  ICoffeeTable createCoffeeTable();
+}
+
+class VictorianFurnitureFactory : FurnitureFactory
+{
+    public  IChair createChair() => new VictorianChair();
+
+    public  ICoffeeTable createCoffeeTable() => new VictorianCoffeeTable();
+
+    public  ISofa createCoffeeTable() => new VictorianSofa();
+}
+
+class ModernFurnitureFactory : FurnitureFactory
+{
+    public  IChair createChair() => new ModernChair();
+
+    public  ICoffeeTable createCoffeeTable() => new ModernCoffeeTable();
+
+    public  ISofa createSofa() => new Modernsofa();
+}
+
---
+++ Patterns/Program.cs
@@ -0,0 +1,52 @@
+﻿public interface IButton
+{
+    void render();
+    void OnClick();
+}
+
+public class WindowsButton : IButton
+{
+    public void OnClick() => Console.WriteLine("Windows btn");
+    public void render() => Console.WriteLine("Windows render");
+}
+
+public class HtmlButton : IButton
+{
+    public void OnClick() => Console.WriteLine("Html btn");
+    public void render() => Console.WriteLine("Html render");
+}
+
+
+public abstract class Dialog
+{
+    public void render()
+    {
+        IButton okButton = CreateButton();
+        okButton.OnClick();
+        okButton.render();
+    }
+
+    public abstract IButton CreateButton();
+}
+
+public class WindowsDialog : Dialog
+{
+    public override IButton CreateButton() => new WindowsButton();
+}
+
+public class WebDialog : Dialog
+{
+    public override IButton CreateButton() => new HtmlButton();
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Dialog dialog = new WindowsDialog();
+        dialog.render();
+
+        dialog = new WebDialog();
+        dialog.render();
+    }
+}
